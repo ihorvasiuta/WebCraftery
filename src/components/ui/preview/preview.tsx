6ab5a2b804c32b1c7851a29695a8f96f7fc36701
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-07-09 23:58:18 +0200

    added Adress bars for iphone and mac preview windows & added status menu of mac to big preview window & added buttons to the iphone preview model

--- src/components/ui/preview/preview.tsx
+++ src/components/ui/preview/preview.tsx
@@ -43,13 +43,106 @@ const RenderPreview: React.FC<PreviewProps> = ({ codes }) => {
 
   return (
     <div className="preview_window_big">
-      <div className="status_bar">
-        <img className="time_img" src={require(`./time_iphone.png`)} alt="" />
-        <img
-          className="mob_wifi_batt_img"
-          src={require(`./status_bar.png`)}
-          alt=""
-        />
+      <div className="actionbar">
+        <div className="leftActions">
+          <div className="item" data-trigger="sPanelLogOptions"></div>
+          <div className="group" data-app="finder">
+            <div className="item bold" data-trigger="sPanelAppFinderOptions">
+              Finder
+            </div>
+            <div className="item" data-trigger="sPanelAppFinderOptionFile">
+              File
+            </div>
+            <div className="item" data-trigger="sPanelAppFinderOptionEdit">
+              Edit
+            </div>
+            <div className="item" data-trigger="sPanelAppFinderOptionView">
+              View
+            </div>
+            <div className="item" data-trigger="sPanelAppFinderOptionGo">
+              Go
+            </div>
+            <div className="item" data-trigger="sPanelAppFinderOptionWindow">
+              Window
+            </div>
+            <div className="item" data-trigger="sPanelAppFinderOptionHelp">
+              Help
+            </div>
+          </div>
+        </div>
+        <div className="rightActions">
+          <div
+            className="item"
+            data-trigger="sPanelBluetooth"
+            data-connected=""
+          ></div>
+          <div className="item" data-trigger="sPanelPower" data-percentage="74">
+            <i></i>
+          </div>
+          <div className="item sActionbarTime" data-trigger="sPanelTime">
+            2:35
+          </div>
+          <div
+            className="item sActionbarNotifications hasNotifications"
+            data-trigger="sPanelNotifications"
+          ></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="circles">
+          <div className="c"></div>
+          <div className="c"></div>
+          <div className="c"></div>
+        </div>
+
+        <div className="browser">
+          <div className="chevrons">
+            <svg
+              viewBox="0 0 20 20"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+              data-name="20"
+              id="_20"
+            >
+              <path
+                transform="translate(6.25 3.75)"
+                d="M0,6.25,6.25,0l.875.875L1.75,6.25l5.375,5.375L6.25,12.5Z"
+                id="Fill"
+              ></path>
+            </svg>
+            <svg
+              viewBox="0 0 20 20"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+              data-name="20"
+              id="_20"
+            >
+              <path
+                transform="translate(6.625 3.75)"
+                d="M7.125,6.25.875,12.5,0,11.625,5.375,6.25,0,.875.875,0Z"
+                id="Fill"
+              ></path>
+            </svg>
+          </div>
+          <div className="search-bar">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="7.89"
+              height="7.887"
+              viewBox="0 0 16.89 16.887"
+            >
+              <path
+                id="Fill"
+                d="M16.006,16.887h0l-4.743-4.718a6.875,6.875,0,1,1,.906-.906l4.719,4.744-.88.88ZM6.887,1.262a5.625,5.625,0,1,0,5.625,5.625A5.631,5.631,0,0,0,6.887,1.262Z"
+                transform="translate(0.003 0)"
+              ></path>
+            </svg>
+            webcraftery.com
+            <div></div>
+          </div>
+        </div>
       </div>
       <iframe
         className="big_iframe"
@@ -60,7 +153,23 @@ const RenderPreview: React.FC<PreviewProps> = ({ codes }) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <style>${codes.css}</style>
+<style>
+      ::-webkit-scrollbar {
+        width: 5px;
+      }
+      ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        background: #888;
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+      }
+      ${codes.css}
+    </style>
     <title>Responsive bottom navigation</title>
   </head>
   <body>
--- src/components/ui/preview/preview_phone.tsx
+++ src/components/ui/preview/preview_phone.tsx
@@ -44,26 +44,66 @@ const RenderPreviewPhone: React.FC<PreviewProps> = ({ codes }) => {
 
   return (
     <div className="iphone">
-      <div className="screen">
-        <div className="status_bar">
-          <img className="time_img" src={require(`./time_iphone.png`)} alt="" />
-          <img
-            className="mob_wifi_batt_img"
-            src={require(`./status_bar.png`)}
-            alt=""
-          />
-        </div>
-        <div className="preview_window">
-          <iframe
-            className="iframe_window"
-            srcDoc={`
+      <div className="btn1"></div>
+      <div className="btn2"></div>
+      <div className="btn3"></div>
+      <div className="iphone_inner">
+        <div className="screen">
+          <div className="status_bar">
+            <img
+              className="time_img"
+              src={require(`./time_iphone.png`)}
+              alt=""
+            />
+            <img
+              className="mob_wifi_batt_img"
+              src={require(`./status_bar.png`)}
+              alt=""
+            />
+          </div>
+          <div className="browser-bar">
+            <img
+              className="translate_img"
+              src={require(`./translate.png`)}
+              alt=""
+            />
+            <div className="url-input">
+              <img className="lock_img" src={require(`./lock.png`)} alt="" />
+              <span className="url-text">webcraftery.com</span>
+            </div>
+            <img
+              className="refresh_img"
+              src={require(`./refresh_icon.png`)}
+              alt=""
+            />
+          </div>
+          <div className="preview_window">
+            <iframe
+              className="iframe_window"
+              srcDoc={`
   <!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <style>${codes.css}</style>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+    <style>
+    ::-webkit-scrollbar {
+        width: 5px;
+      }
+      ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        background: #888;
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+      }
+      ${codes.css}
+      </style>
     <title>Responsive bottom navigation</title>
   </head>
   <body>
@@ -89,15 +129,16 @@ document.addEventListener('DOMContentLoaded', function() {
   </body>
   </html>
 `}
-            style={{ width: "100%", height: "420px" }}
-            sandbox="allow-scripts"
-            title="Preview Window"
-          />
+              style={{ width: "100%", height: "396px" }}
+              sandbox="allow-scripts"
+              title="Preview Window"
+            />
+          </div>
         </div>
-      </div>
-      <div className="notch">
-        <div className="camera"></div>
-        <div className="speaker"></div>
+        <div className="notch">
+          <div className="camera"></div>
+          <div className="speaker"></div>
+        </div>
       </div>
     </div>
   );
