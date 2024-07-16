ad4273edb15cd446d1c22ba60eb791a05bb0b4f5
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-07-16 02:07:16 +0200

    added, styled MIT License part in the detalils block, styling not finished...

--- src/components/ui/element_details_block/detailsblock.tsx
+++ src/components/ui/element_details_block/detailsblock.tsx
@@ -8,8 +8,8 @@ type DetailsData = {
   url: string;
 };
 type DetailsProps = {
-    details_data: DetailsData;
-}
+  details_data: DetailsData;
+};
 const DetailsBlock: React.FC<DetailsProps> = ({ details_data }) => {
   return (
     <div className="details_block">
@@ -26,14 +26,17 @@ const DetailsBlock: React.FC<DetailsProps> = ({ details_data }) => {
           <div className="fr_action_block">
             <p>Add this to the head of html: </p>
             <input
-            className="value_imput"
+              className="value_imput"
               type="text"
               value="<link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>"
               readOnly
             />
           </div>
           <div className="sec_action_block">
-            <p>In this element were used icons from boxicons so please don’t forget to add this link to your html head.</p>
+            <p>
+              In this element were used icons from boxicons so please don’t
+              forget to add this link to your html head.
+            </p>
           </div>
         </div>
       </div>
@@ -49,6 +52,47 @@ const DetailsBlock: React.FC<DetailsProps> = ({ details_data }) => {
         >
           https://example.com/classic-menu
         </a>
+      </div>
+      <div className="mit_l_bl">
+        <div className="ttl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="mit_b_ic"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M12 2v1.75M16 21h-4m-4 0h4m9-16a4.821 4.821 0 0 1-4.055.12l-1.583-.68A8.532 8.532 0 0 0 12 3.75M3 5a4.821 4.821 0 0 0 4.055.12l1.583-.68A8.532 8.532 0 0 1 12 3.75M12 21V3.75m7 1.75-2.892 6.145a3.196 3.196 0 1 0 5.784 0L19 5.5Zm-14 0-2.892 6.145a3.196 3.196 0 1 0 5.784 0L5 5.5Z"></path>
+          </svg>
+          <p className="mit_tt_tx">MIT License</p>
+        </div>
+        <textarea
+          name=""
+          id=""
+          className="mit_tx_ara custom_scrollbar"
+          readOnly
+        >
+          Copyright - 2024 Rodrypaladin (David Rodríguez) Permission is hereby
+          granted, free of charge, to any person obtaining a copy of this
+          software and associated documentation files (the “Software”), to deal
+          in the Software without restriction, including without limitation the
+          rights to use, copy, modify, merge, publish, distribute, sublicense,
+          and/or sell copies of the Software, and to permit persons to whom the
+          Software is furnished to do so, subject to the following conditions:
+          <br />
+          &nbsp The above copyright notice and this permission notice shall be
+          included in all copies or substantial portions of the Software. THE
+          SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+          MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+          IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+          CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+          TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+          SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </textarea>
       </div>
     </div>
   );
