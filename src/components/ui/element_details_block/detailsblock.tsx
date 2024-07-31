0ef89a9204d45885769c96dfc02d08352e529d18
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-07-31 23:43:56 +0200

    added some ui changes to details block

--- src/components/ui/element_details_block/detailsblock.tsx
+++ src/components/ui/element_details_block/detailsblock.tsx
@@ -56,11 +56,16 @@ SOFTWARE.
       <div className="important_part">
         <div className="title_important">
           <h1 className="important_text">Important</h1>
-          <img
+          <svg
             className="important_icon"
-            src="/w_icons/error_24dp_EA3323_FILL0_wght400_GRAD0_opsz24.png"
-            alt=""
-          />
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#EA3323"
+          >
+            <path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
         </div>
         {details_data.important && (
           <div
@@ -134,9 +139,9 @@ SOFTWARE.
               className="mit_b_ic"
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
             >
               <path d="M12 2v1.75M16 21h-4m-4 0h4m9-16a4.821 4.821 0 0 1-4.055.12l-1.583-.68A8.532 8.532 0 0 0 12 3.75M3 5a4.821 4.821 0 0 0 4.055.12l1.583-.68A8.532 8.532 0 0 1 12 3.75M12 21V3.75m7 1.75-2.892 6.145a3.196 3.196 0 1 0 5.784 0L19 5.5Zm-14 0-2.892 6.145a3.196 3.196 0 1 0 5.784 0L5 5.5Z"></path>
             </svg>
