24d26346f322286a2d244b01c816a672cd6c13bc
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-07-31 23:34:10 +0200

    added Coming soon as current content of inspirationspage

---
+++ src/components/pages/inspirationspage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./inspirationspage.css";
+
+const InspirationsPage: React.FC = () => {
+
+  return (
+    <div className="inspirationspage">
+      <div className="com_son_block">
+        <div className="coming_soon_title">Coming Soon...</div>
+        <p className="coming_soon_txt">
+          We're working on our website. Join our newsletter and get notifed.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InspirationsPage;
