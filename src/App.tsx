4ff2387c06654794ae45860e1b38ffed58fc310b
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-07-31 23:43:19 +0200

    added inspirationpage component, involved into a action

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/common/header";
 import Footer from "./components/common/footer";
 import HomePage from "./components/pages/homepage";
 import ElementsPage from "./components/pages/elementspage";
+import InspirationsPage from "./components/pages/inspirationspage";
 
 const App: React.FC = () => {
   return (
@@ -14,7 +15,7 @@ const App: React.FC = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/elements" element={<ElementsPage />} />
           <Route path="/elements/:elementId" element={<ElementsPage />} />
-          <Route path="/inspirations" element={"<InspirationsPage />"} />
+          <Route path="/inspirations" element={<InspirationsPage />} />
           <Route path="/usage" element={"<UsagePage />"} />
         </Routes>
       </main>
