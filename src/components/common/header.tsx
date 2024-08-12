eafb41db78e871c764b5b03b6c2bc26eb2804c09
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-08-13 00:08:34 +0200

    added power icon to the builder link in the navigation menu

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -77,6 +77,19 @@ const Header: React.FC = () => {
                   isActive ? "nav__link active" : "nav__link"
                 }
               >
+                <svg
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="css-i6dzq1"
+                >
+                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
+                </svg>
                 Builder
               </NavLink>
             </li>
